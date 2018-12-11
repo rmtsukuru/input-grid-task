bbd4481c1887c715b326dd2ca13f6eb02083caa0
rmtsukuru <samcollegeemail@gmail.com>
2018-12-10 23:07:23 -0500

    Fixed number formatting.

--- summing-grid/src/utils.js
+++ summing-grid/src/utils.js
@@ -1,6 +1,18 @@
 function formatNumber(x) {
-    // TODO fix this
-    return x;
+    let result = x.toPrecision(3);
+    if (Math.abs(x) >= 1000000000000) {
+        result = (x / 1000000000000).toPrecision(3) + 'T';
+    }
+    else if (Math.abs(x) >= 1000000000) {
+        result = (x / 1000000000).toPrecision(3) + 'B';
+    }
+    else if (Math.abs(x) >= 1000000) {
+        result = (x / 1000000).toPrecision(3) + 'M';
+    }
+    else if (Math.abs(x) >= 1000) {
+        result = (x / 1000).toPrecision(3) + 'K';
+    }
+    return result.includes('.') ? result.replace(/\.?0+($|T$|B$|M$|K$)/, '$1') : result;
 }
 
 export {
